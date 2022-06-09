afee432084768d70b5078b0f402b0dab32edb9f5
정희망 <hope_03_21@naver.com>
2022-06-09 11:15:38 +0900

    퍼스트 커밋

---
+++ scripts/cmd_turtle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+class WhereToGo():
+    def __init__(self):
+        rospy.init_node('order_to_turtle')
+        rospy.Subscriber('sub_string',String, self.string_callback)
+        self.turtle_cmd_vel_pub = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size=10)
+    
+# ==================================================
+#                 Callback Functions
+# ==================================================
+    def string_callback(self,_string_msg=String()):
+        twist_msg = Twist()
+
+        if _string_msg.data == "go":
+            twist_msg.linear.x = 2.0
+            twist_msg.angular.z = 0.0
+        
+        elif _string_msg.data == "left":
+            twist_msg.linear.x = 2.0
+            twist_msg.angular.z = 1.5
+            
+        elif _string_msg.data == "right":
+            twist_msg.linear.x = 2.0
+            twist_msg.angular.z = -1.5
+        
+        else:
+            rospy.logwarn("input String should be 'go', 'left', 'right', try again")
+            
+        self.turtle_cmd_vel_pub.publish(twist_msg)
+
+def run():
+    new_class = WhereToGo()
+    rospy.loginfo("Initialized")
+    rospy.spin()
+    
+if __name__ == "__main__":
+    try:
+        run()
+    except Exception as e:
+        rospy.logwarn(e)
+        
